353418254265f774e8a6247027c85595a5013a79
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2016-12-20 18:18:14 +0600

    * Trie: Added Trie Node.

---
+++ Trie/Trie/Trie/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+class TrieNode
+{
+public:
+	TrieNode() 
+	{
+
+	}
+
+protected:
+	TrieNode* nodes[ALPHABET_SIZE];
+};
+
+int main()
+{
+	return 0;
+}
